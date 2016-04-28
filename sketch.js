1eac23e3f4b5a2cb4c23f6da6a8bdaaf8cbe2f7d
Simon Posada Fishman <simon.posada29@gmail.com>
2016-04-27 20:31:43 -0700

    Add specific circle sizing

--- sketch.js
+++ sketch.js
@@ -1,56 +1,31 @@
-var fileSelect;
-var drawButton;
-var img, img_w, img_h;
+var img, img_w, img_h, circles, row;
 
 function setup() {
     var canvas = createCanvas(50, 50);
     canvas.parent('out');
-    // fileSelect = createFileInput(gotFile);
     imageMode(CENTER);
 }
 
 function table() {
     resizeCanvas(0, 0);
-    var max_diam = parseInt($('#max_diam').val());
-    var draw_diam = parseInt($('#draw_diam').val());
-    var circles = [];
 
-    img.loadPixels();
-    var pxls = img.pixels;
-    var rows = 0;
-    var x = floor(max_diam / 2.0);
-    var y = floor(max_diam / 2.0);
-    var c, diam;
-    var d = pixelDensity();
-
-    for (var i = x * 4 * d; i < pxls.length; i += max_diam * 4 * d) {
-        c = color(pxls[i], pxls[i + 1], pxls[i + 2]);
-        diam = draw_diam - brightness(c) / 255.0 * draw_diam;
-        circles.push(new Circle(x, y, diam, color(0)));
-        x += max_diam;
-        if (x >= img.width) {
-            rows++;
-            x = floor(max_diam / 2.0);
-            i = x * 4 * d + img.width * 4 * d * rows * max_diam;
-            y += max_diam;
-        }
-    }
+    load_circles();
 
     background(255);
     var table = $("#diam_table");
     table.empty();
     table.show();
-    var rowLength = circles.length / rows;
+    var rowLength = circles.length / row;
     table.append("<tr></tr>");
     activeRow = table.children().children().last();
 
-    activeRow.append("<td>"+ floor(circles[0].d) +"</td>");
+    activeRow.append("<td>" + floor(circles[0].d) + "</td>");
     for (i = 1; i < circles.length; i++) {
         if (i % rowLength == 0) {
             table.append("<tr></tr>");
             activeRow = table.children().children().last();
         }
-        activeRow.append("<td>"+ floor(circles[i].d) +"</td>");
+        activeRow.append("<td>" + floor(circles[i].d) + "</td>");
     }
 }
 
@@ -79,25 +54,51 @@ function img_loaded() {
 }
 
 function half_tone() {
-
     $("#diam_table").hide();
 
     resizeCanvas(img.width, img.height);
+
+    load_circles();
+
+    background(255);
+    for (i = 0; i < circles.length; i++) {
+        circles[i].display();
+    }
+}
+
+function load_circles(){
     var max_diam = parseInt($('#max_diam').val());
     var draw_diam = parseInt($('#draw_diam').val());
-    var circles = [];
+
+    var numcircles = 4;
+    var minsize = 1;
+    var maxsize = 4;
+    circles = [];
 
     img.loadPixels();
     var pxls = img.pixels;
-    var row = 0;
+    row = 0;
     var x = floor(max_diam / 2.0);
     var y = floor(max_diam / 2.0);
-    var c, diam;
+    var c, darkness, diam;
     var d = pixelDensity();
 
     for (var i = x * 4 * d; i < pxls.length; i += max_diam * 4 * d) {
         c = color(pxls[i], pxls[i + 1], pxls[i + 2]);
-        diam = draw_diam - brightness(c) / 255.0 * draw_diam;
+
+        // Value from 0 to 1 (1 being the darkest)
+        darkness = 1 - brightness(c) / 256.0 ;
+
+        var diff;
+        for (var j = 0; j <= 1; j += 1/(numcircles-1)) {
+            diff = abs(j - darkness)
+            if (diff <= (1/(numcircles-1))/2) {
+                darkness = j;
+            }
+        }
+
+        diam = map(darkness, 0, 1, minsize, maxsize);
+
         circles.push(new Circle(x, y, diam, color(0)));
         x += max_diam;
         if (x >= img.width) {
@@ -106,11 +107,6 @@ function half_tone() {
             i = x * 4 * d + img.width * 4 * d * row * max_diam;
             y += max_diam;
         }
-    }
-
-    background(255);
-    for (i = 0; i < circles.length; i++) {
-        circles[i].display();
     }
 }
 
